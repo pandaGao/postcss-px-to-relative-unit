0aa65ae72b1522ac5ec55cff075ab124cc1935b8
张镇坤 <972997077@qq.com>
2019-06-11 12:08:05 +0800

    增加元素过滤vw单位 &&更新测试用例

--- test/index.test.js
+++ test/index.test.js
@@ -4,95 +4,104 @@ const path = require('path')
 const postcss = require('postcss')
 const pxToUnit = require('../index')
 
-function runCase (input, output, options) {
-  let inputFile = fs.readFileSync(input)
-  let outputFile = fs.readFileSync(output, 'utf8')
-  return postcss([pxToUnit(options)]).process(inputFile, {
-    from: input
-  }).then(function (result) {
-    expect(result.css).toEqual(outputFile)
-    expect(result.warnings()).toHaveLength(0)
-  })
+function runCase(input, output, options) {
+    let inputFile = fs.readFileSync(input)
+    let outputFile = fs.readFileSync(output, 'utf8')
+    return postcss([pxToUnit(options)]).process(inputFile, {
+        from: input
+    }).then(function(result) {
+        expect(result.css).toEqual(outputFile)
+        expect(result.warnings()).toHaveLength(0)
+    })
 }
 
 describe('Convert', () => {
-  test('px to rem', () => {
-    let inputFile = path.resolve(__dirname, './input/test.css')
-    let outputFile = path.resolve(__dirname, './output/rem.css')
-    return runCase(inputFile, outputFile, {
-      targetUnit: 'rem'
+    test('px to rem', () => {
+        let inputFile = path.resolve(__dirname, './input/test.css')
+        let outputFile = path.resolve(__dirname, './output/rem.css')
+        return runCase(inputFile, outputFile, {
+            targetUnit: 'rem'
+        })
     })
-  })
 
-  test('px to vw', () => {
-    let inputFile = path.resolve(__dirname, './input/test.css')
-    let outputFile = path.resolve(__dirname, './output/vw.css')
-    return runCase(inputFile, outputFile)
-  })
-
-  test('px to vw&rem', () => {
-    let inputFile = path.resolve(__dirname, './input/test.css')
-    let outputFile = path.resolve(__dirname, './output/vw&rem.css')
-    return runCase(inputFile, outputFile, {
-      targetUnit: 'vw&rem'
+    test('px to vw', () => {
+        let inputFile = path.resolve(__dirname, './input/test.css')
+        let outputFile = path.resolve(__dirname, './output/vw.css')
+        return runCase(inputFile, outputFile)
     })
-  })
 
-  test('ignore threshold', () => {
-    let inputFile = path.resolve(__dirname, './input/test.css')
-    let outputFile = path.resolve(__dirname, './output/ignore.css')
-    return runCase(inputFile, outputFile, {
-      targetUnit: 'rem',
-      ignoreThreshold: 5
+    test('px to vw&rem', () => {
+        let inputFile = path.resolve(__dirname, './input/test.css')
+        let outputFile = path.resolve(__dirname, './output/vw&rem.css')
+        return runCase(inputFile, outputFile, {
+            targetUnit: 'vw&rem',
+            ignoreThreshold: 1,
+            viewportWidth: 750,
+            viewportHeight: 1334,
+            htmlFontSize: 75,
+            unitPrecision: 5,
+            excludeFiles: [],
+            excludeSelectors: [],
+            excludeProperties: [],
+            excludeVwProperties: [/^font-size$/]
+        })
     })
-  })
 
-  test('ignore pattern', () => {
-    let inputFile = path.resolve(__dirname, './input/ignore-pattern.css')
-    let outputFile = path.resolve(__dirname, './input/ignore-pattern.css')
-    return runCase(inputFile, outputFile)
-  })
+    test('ignore threshold', () => {
+        let inputFile = path.resolve(__dirname, './input/test.css')
+        let outputFile = path.resolve(__dirname, './output/ignore.css')
+        return runCase(inputFile, outputFile, {
+            targetUnit: 'rem',
+            ignoreThreshold: 5
+        })
+    })
+
+    test('ignore pattern', () => {
+        let inputFile = path.resolve(__dirname, './input/ignore-pattern.css')
+        let outputFile = path.resolve(__dirname, './input/ignore-pattern.css')
+        return runCase(inputFile, outputFile)
+    })
 })
 
 describe('Exclude rules', () => {
-  test('exclude file', () => {
-    let inputFile = path.resolve(__dirname, './input/test.css')
-    let outputFile = path.resolve(__dirname, './input/test.css')
-    return runCase(inputFile, outputFile, {
-      excludeFiles: [/test/]
+    test('exclude file', () => {
+        let inputFile = path.resolve(__dirname, './input/test.css')
+        let outputFile = path.resolve(__dirname, './input/test.css')
+        return runCase(inputFile, outputFile, {
+            excludeFiles: [/test/]
+        })
     })
-  })
 
-  test('exclude selector (regexp)', () => {
-    let inputFile = path.resolve(__dirname, './input/test.css')
-    let outputFile = path.resolve(__dirname, './input/test.css')
-    return runCase(inputFile, outputFile, {
-      excludeSelectors: [/test/]
+    test('exclude selector (regexp)', () => {
+        let inputFile = path.resolve(__dirname, './input/test.css')
+        let outputFile = path.resolve(__dirname, './input/test.css')
+        return runCase(inputFile, outputFile, {
+            excludeSelectors: [/test/]
+        })
     })
-  })
 
-  test('exclude selector (string)', () => {
-    let inputFile = path.resolve(__dirname, './input/test.css')
-    let outputFile = path.resolve(__dirname, './input/test.css')
-    return runCase(inputFile, outputFile, {
-      excludeSelectors: ['test']
+    test('exclude selector (string)', () => {
+        let inputFile = path.resolve(__dirname, './input/test.css')
+        let outputFile = path.resolve(__dirname, './input/test.css')
+        return runCase(inputFile, outputFile, {
+            excludeSelectors: ['test']
+        })
     })
-  })
 
-  test('exclude property (regexp)', () => {
-    let inputFile = path.resolve(__dirname, './input/test.css')
-    let outputFile = path.resolve(__dirname, './output/exclude-property.css')
-    return runCase(inputFile, outputFile, {
-      excludeProperties: [/^width$/]
+    test('exclude property (regexp)', () => {
+        let inputFile = path.resolve(__dirname, './input/test.css')
+        let outputFile = path.resolve(__dirname, './output/exclude-property.css')
+        return runCase(inputFile, outputFile, {
+            excludeProperties: [/^width$/]
+        })
     })
-  })
 
-  test('exclude property (string)', () => {
-    let inputFile = path.resolve(__dirname, './input/test.css')
-    let outputFile = path.resolve(__dirname, './output/exclude-property-string.css')
-    return runCase(inputFile, outputFile, {
-      excludeProperties: ['width']
+    test('exclude property (string)', () => {
+        let inputFile = path.resolve(__dirname, './input/test.css')
+        let outputFile = path.resolve(__dirname, './output/exclude-property-string.css')
+        return runCase(inputFile, outputFile, {
+            excludeProperties: ['width']
+        })
     })
-  })
 
 })
